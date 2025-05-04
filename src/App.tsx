9a172bf9787aa80e674e72f578e0914975700ae7
Adonis <adonis.yu@cvsu.edu.ph>
2025-05-04 16:47:47 +0800

    cleaned up files, added shadcn

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-center gap-40">
+      <h1 className="text-9xl font-black">Hello Guys</h1>
+    </main>
+  );
+}
+
+export default App;
